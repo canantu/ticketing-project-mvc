0fe527fd39650af962150bf84168d4376deb477f
canantu <tunc.canan84@gmail.com>
2024-04-01 16:46:09 +0200

    [+] Converter

--- src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,6 +1,8 @@
 package com.cydeo.bootstrap;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.enums.Gender;
 import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.boot.CommandLineRunner;
@@ -27,6 +29,32 @@ public class DataGenerator implements CommandLineRunner {
         roleService.save(adminRole);
         roleService.save(managerRole);
         roleService.save(employeeRole);
+
+        UserDTO user1 = new UserDTO("John", "Kesy",
+                "john@cydeo.com", "Abc1", "7459684532", true, managerRole, Gender.MALE);
+        UserDTO user5 = new UserDTO("Mike", "Smith",
+                "mike@cydeo.com", "Abc2",  "7459684532", true, adminRole, Gender.MALE);
+        UserDTO user2 = new UserDTO("Delisa",
+                "Norre", "delisa@cydeo.com", "123", "8567412358", true, managerRole, Gender.FEMALE);
+        UserDTO user3 = new UserDTO("Craig", "Jark",
+                "craig@cydeo.com", "Abc3", "7777775566", true, employeeRole, Gender.MALE);
+        UserDTO user4 = new UserDTO("Shaun",
+                "Hayns", "shaun@cydeo.com", "Abc4", "3256987412",true,  managerRole, Gender.MALE);
+        UserDTO user6 = new UserDTO("Elizebeth",
+                "Loren", "elizebeth@cydeo.com", "Abc4", "5306987412", true, employeeRole, Gender.FEMALE);
+        UserDTO user7 = new UserDTO("Maria",
+                "Ada", "maria@cydeo.com", "Abc4",  "9996987412", true, employeeRole, Gender.FEMALE);
+        UserDTO user8 = new UserDTO("Bill",
+                "Matt", "bill@cydeo.com", "Abc4", "8881239846", true, employeeRole, Gender.MALE);
+
+        userService.save(user1);
+        userService.save(user2);
+        userService.save(user3);
+        userService.save(user4);
+        userService.save(user5);
+        userService.save(user6);
+        userService.save(user7);
+        userService.save(user8);
 
 
 
