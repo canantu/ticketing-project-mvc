06113ed06e95c752e36cf2ee23363895aac91b95
canantu <tunc.canan84@gmail.com>
2024-04-18 00:28:15 +0200

    [+] Create Project Page

--- src/main/java/com/cydeo/enums/Status.java
+++ src/main/java/com/cydeo/enums/Status.java
@@ -2,7 +2,7 @@ package com.cydeo.enums;
 
 public enum Status {
 
-    OPEN("Open"), IN_PROGRESS("In_Progress"), COMPLETED("Completed");
+    OPEN("Open"), IN_PROGRESS("In Progress"), COMPLETED("Completed");
 
     private final String value;
 
